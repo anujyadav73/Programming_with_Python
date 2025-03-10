e0f59f980296c36ced5979c8be5235274d5634b4
Anuj Yadav <anujyadav91188@gmail.com>
2025-03-10 16:31:16 +0530

    word_replacement

---
+++ word_replacement.py
@@ -0,0 +1,6 @@
+sentence = input("Enter sentence:")
+print("Original Sentence:", sentence)
+
+word1 = input("Enter word to replace:")
+word2 = input("Enter word to replace with:")
+print("Replaced Sentence:", sentence.replace(word1, word2))
